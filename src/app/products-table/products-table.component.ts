26f255de023aa108767c6947aa4d00d763fda0e1
BrunetoJG <brunojuni@yahoo.com.br>
2024-09-18 20:07:27 -0300

    first commit and ngFor

---
+++ src/app/products-table/products-table.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-products-table',
+  templateUrl: './products-table.component.html',
+  styleUrl: './products-table.component.css'
+})
+export class ProductsTableComponent{
+
+products: Product[] = [
+  {
+    id: 1,
+    name: "Mouse",
+    price: 500,
+    category: "Eletrônicos"
+  },
+  {
+    id: 2,
+    name: "Processador",
+    price: 4000,
+    category: "Eletrônicos"
+  },
+  {
+    id: 3,
+    name: "Teclado",
+    price: 3000,
+    category: "Eletrônicos"
+  }
+];
+
+}
