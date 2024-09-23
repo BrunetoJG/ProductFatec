ec27f761eaf07c9c1a87226a9c676afab5e39a0b
BrunetoJG <brunojuni@yahoo.com.br>
2024-09-23 17:09:41 -0300

    Json integration

--- src/app/products-table/products-table.component.ts
+++ src/app/products-table/products-table.component.ts
@@ -8,27 +8,5 @@ import { Product } from '../product';
 })
 export class ProductsTableComponent{
 
-products: Product[] = [
-  {
-    id: 1,
-    name: "Mouse",
-    price: 500,
-    category: "Eletrônicos"
-  },
-  {
-    id: 2,
-    name: "Processador",
-    price: 4000,
-    category: "Eletrônicos"
-  },
-  {
-    id: 3,
-    name: "Teclado",
-    price: 3000,
-    category: "Eletrônicos"
-  }
-];
-remove(removeProduct:Product){
-  this.products = this.products.filter( b => b !== removeProduct);
-}
+
 }
